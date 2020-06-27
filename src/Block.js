e5f0c990703c47a06e2e9586a72152b744f3b68e
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-06-27 16:28:50 -0400

    So close, attempt using CORS anywhere

--- src/Block.js
+++ src/Block.js
@@ -4,57 +4,57 @@ import { getStyles } from "./getStyles"
 import { defaultConfig } from "./configs"
 
 const Block = (props) => {
-  // Get the custom Aphrodite styles to be used in this Block:
+  // Get the custom Aphrodite styles that will be used in this Block:
   const classes = StyleSheet.create(getStyles(props))
-  // Load the helper function that will join Atomic CSS classes with Aphrodite:
+  // Load the Element SDK helper to join Atomic CSS classes with Aphrodite:
   const joinClasses = props.joinClasses
-  // Destructure the HTML embed code from the data props:
+  // Destructure the oEmbed code snippets (HTML) from the data props:
   const [embedTweetHTML, embedInstagramHTML] = props.data
 
-  // React's Effect Hook (useEffect) will run code when the Block is rendered:
+  // Run the code once when the Block is rendered with React's Effect Hook:
   useEffect(() => {
-    // Add a link to the external Twitter script file:
+    // Load Twitter's widgets.js, the external JavaScript file for Twitter:
     const twitterScript = document.createElement("script")
     twitterScript.setAttribute("src", "https://platform.twitter.com/widgets.js")
     twitterScript.setAttribute("async", true)
     const tweetElement = document.getElementsByClassName("twitter-tweet")[0]
-    // Only add the Twitter script if there is a Tweet to embed:
+    // Only add the Twitter file if there is a Tweet on the page to embed:
     tweetElement ? tweetElement.appendChild(twitterScript) : null
 
-    // Add a link to the external Instagram script file:
+    // Load Instagram's embed.js, the external JavaScript file for Instagram:
     const instagramScript = document.createElement("script")
     instagramScript.setAttribute("src", "https://www.instagram.com/embed.js")
     instagramScript.setAttribute("async", true)
     const instaElement = document.getElementsByClassName("instagram-media")[0]
-    // Only add the Instagram  script if there is an Instagram Post to embed:
+    // Only add the Instagram file if there is an Instagram Post to embed:
     instaElement ? instaElement.appendChild(instagramScript) : null
   }, [])
-  // Passing [] as useEffect's second parameter will run the code just once.
+  // Using [] for useEffect's second argument will run the code only once.
 
-  // Combine the Tweet and the Instagram Post into a single HTML string:
+  // Combine the HTML for the Tweet and the Instagram Post into one string:
   const embedHTML = embedTweetHTML + embedInstagramHTML
-  // For HTML markup, React requires an object with the __html property:
+  // To display HTML markup, React needs an object with the __html property:
   const outputAsMarkup = { __html: embedHTML }
 
-  // Make a <div> element based on the banner text using the custom styles:
+  // Create a <div> element with the banner text using the custom styles:
   const outputDiv = (
     <div
-      // Combine Atomic CSS classes with custom styles using joinClasses:
+      // Use joinClasses to combine Atomic CSS classes with custom styles:
       className={joinClasses(
         "ma0 pa3 flex justify-around",
         css(classes.banner)
       )}
-      // The class ma0 sets all margins to 0, and pa3 sets padding to 1rem.
-      // flex & justify-around enable CSS flexbox with space around the items.
+      // The Atomic CSS classes ma0 and pa3 set margins to 0 & padding to 1rem,
+      // while flex & justify-around have CSS flexbox place space around items.
 
-      // dangerouslySetInnerHTML will render HTML markup, except <script> tags:
+      // The dangerouslySetInnerHTML prop will render HTML except <script> tags
       dangerouslySetInnerHTML={outputAsMarkup}
     ></div>
   )
 
-  // Google AMP pages can't load external JavaScript, so the social media embed
-  // won't be able to load on the AMP version of the Block. For the AMP page,
-  // props.utils.isAmpRequest is true, so return null to render an empty Block:
+  // Google AMP pages won't load external JavaScript files, so this social
+  // media embed Block won't be able to load on the AMP version. For the AMP
+  // page, props.utils.isAmpRequest is true, so return null for an empty Block:
   return props.utils.isAmpRequest ? null : outputDiv
 }
 
--- src/getDataProps.js
+++ src/getDataProps.js
@@ -1,6 +1,7 @@
 export const getDataProps = (utils, props) => {
   // Helper function to use JSONP (JSON with Padding) to bypass cross-origin
-  // request sharing restrictions (CORS restrictions) when querying APIs
+  // request sharing restrictions (CORS restrictions) when querying APIs:
+  /*
   const fetchJSONP = ((unique) => (url) =>
     new Promise((rs) => {
       const script = document.createElement("script")
@@ -23,43 +24,44 @@ export const getDataProps = (utils, props) => {
     }))(0)
   // Source: https://gist.github.com/gf3/132080/110d1b68d7328d7bfe7e36617f7df85679a08968#gistcomment-2090652
 
-  // Use JSONP for Twitter: Twitter's API does not allow CORS requests.
+  // Use JSONP for Twitter, because Twitter's API does not allow CORS requests:
   const twitterPromise = fetchJSONP(
     `https://publish.twitter.com/oembed?url=${props.embedTweetURL}`
   )
     .then((res) => res.json())
-    .catch((e) => console.log(`Problem with fetching Twitter result`))
+    .catch((e) => console.log(`Problem with fetching Twitter result: ${e}`))
 
-  // Use JSON for Instagram; there are no CORS problems with Instagram's API.
-  const instagramPromise = fetch(
-    `https://api.instagram.com/oembed?url=${props.embedInstagramURL}`
-  )
+  */
+
+  // Use JSON for Instagram; there are no CORS problems with Instagram's API:
+  const twitterPromise = utils.client
+    .request(
+      // Using the request helper function from the Element SDK (available as
+      // utils.client.request) fixes "ReferenceError: fetch is not defined",
+      // which would occur if you are trying to use fetch on the server side.
+      // The Fetch API (window.fetch) is not defined on the server, since there
+      // is no window object outside of the browser. Request acts like fetch.
+      `https://cors-anywhere.herokuapp.com/` +
+        `https://publish.twitter.com/oembed?url=${props.embedTweetURL}`
+    )
+    .then((res) => res.json())
+    .catch((e) => console.log(`Error when fetching Instagram Post: ${e}`))
+
+  // Use JSON for Instagram; there are no CORS problems with Instagram's API:
+  const instagramPromise = utils.client
+    .request(
+      // Using the request helper function from the Element SDK (available as
+      // utils.client.request) fixes "ReferenceError: fetch is not defined",
+      // which would occur if you are trying to use fetch on the server side.
+      // The Fetch API (window.fetch) is not defined on the server, since there
+      // is no window object outside of the browser. Request acts like fetch.
+      `https://api.instagram.com/oembed?url=${props.embedInstagramURL}`
+    )
     .then((res) => res.json())
     .catch((e) => console.log(`Error when fetching Instagram Post: ${e}`))
 
   return Promise.all([twitterPromise, instagramPromise]).then((values) => [
-    values[0].html,
-    values[1].html,
+    values[0] ? values[0].html : null,
+    values[1] ? values[1].html : null,
   ])
-
-  /* 
-
-    
-  // Destructure the URLs for the Tweet and Instagram Post from the props
-  const { embedTweetURL, embedInstagramURL } = props
-  const embedTweetHTML = fetchJSONP(
-    `https://publish.twitter.com/oembed?url=${embedTweetURL}`
-  )
-    .then((res) => res.json().html)
-    .catch((e) => console.log(`Error when fetching Tweet: ${e}`))
-  const embedInstagramHTML = fetchJSONP(
-    `https://api.instagram.com/oembed?url=${embedInstagramURL}`
-  )
-    .then((res) => res.json().html)
-    .catch((e) => console.log(`Error when fetching Instagram Post: ${e}`))
-
-  // Return a new object containing the embed HTML code for the Tweet and Post
-  return Promise.resolve({ embedTweetHTML, embedInstagramHTML })
-
-  */
 }
