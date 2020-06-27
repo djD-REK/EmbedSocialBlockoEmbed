48090505d0726274ce0ca7a601046d5a416924e6
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-06-27 19:53:55 -0400

    Working version using request, browser extension for CORS (instead of JSONP or proxy), and removing <script></script> tags from JSON data
    Only Twitter needs CORS browser extension

--- src/getDataProps.js
+++ src/getDataProps.js
@@ -1,73 +1,34 @@
-/*import { fetchPonyfill } from "fetch-ponyfill"
-const { Response } = fetchPonyfill({ Promise: Promise })*/
-// The npm package fetch-ponyfill must be installed using
-// npm install --save fetch-ponyfill
-// in order to get access to the Response object for the JSONP code below.
-import { Response } from "node-fetch"
-
 export const getDataProps = (utils, props) => {
-  // Helper function to use JSONP (JSON with Padding) to bypass cross-origin
-  // request sharing restrictions (CORS restrictions) when querying APIs:
-  const fetchJSONP = ((unique) => (url) =>
-    new Promise((rs) => {
-      const script = document.createElement("script")
-      const name = `_jsonp_${unique++}`
-
-      if (url.match(/\?/)) {
-        url += `&callback=${name}`
-      } else {
-        url += `?callback=${name}`
-      }
-
-      script.src = url
-      window[name] = (json) => {
-        rs(new Response(JSON.stringify(json)))
-        script.remove()
-        delete window[name]
-      }
-
-      document.body.appendChild(script)
-    }))(0)
-  // Source: https://gist.github.com/gf3/132080/110d1b68d7328d7bfe7e36617f7df85679a08968#gistcomment-2090652
-
-  // Use JSONP for Twitter, because Twitter's API does not allow CORS requests:
-  const twitterPromise = fetchJSONP(
-    `https://publish.twitter.com/oembed?url=${props.embedTweetURL}`
-  )
-    .then((res) => res.json())
-    .catch((e) => console.log(`Problem with fetching Twitter result: ${e}`))
-
-  /*
-  // Use JSON for Instagram; there are no CORS problems with Instagram's API:
+  // Use JSON for Twitter; you won't be able to view the Tweet in the local
+  // development environment without a CORS browser plugin, like Moesif CORS.
   const twitterPromise = utils.client
-    .request(
-      // Using the request helper function from the Element SDK (available as
-      // utils.client.request) fixes "ReferenceError: fetch is not defined",
-      // which would occur if you are trying to use fetch on the server side.
-      // The Fetch API (window.fetch) is not defined on the server, since there
-      // is no window object outside of the browser. Request acts like fetch.
-      `https://cors-anywhere.herokuapp.com/` +
-        `https://publish.twitter.com/oembed?url=${props.embedTweetURL}`
-    )
-    .then((res) => res.jsonp())
-    .catch((e) => console.log(`Error when fetching Instagram Post: ${e}`))
-*/
+    .request(`https://publish.twitter.com/oembed?url=${props.embedTweetURL}`)
+    .then((res) => res.json())
+    .catch((e) => console.log(`Error when fetching Tweet: ${e}`))
 
   // Use JSON for Instagram; there are no CORS problems with Instagram's API:
   const instagramPromise = utils.client
-    .request(
-      // Using the request helper function from the Element SDK (available as
-      // utils.client.request) fixes "ReferenceError: fetch is not defined",
-      // which would occur if you are trying to use fetch on the server side.
-      // The Fetch API (window.fetch) is not defined on the server, since there
-      // is no window object outside of the browser. Request acts like fetch.
-      `https://api.instagram.com/oembed?url=${props.embedInstagramURL}`
-    )
+    .request(`https://api.instagram.com/oembed?url=${props.embedInstagramURL}`)
     .then((res) => res.json())
     .catch((e) => console.log(`Error when fetching Instagram Post: ${e}`))
 
+  // Using the request helper function from the Element SDK (available as
+  // utils.client.request) fixes "ReferenceError: fetch is not defined",
+  // which would occur if you are trying to use fetch on the server side.
+  // The Fetch API (window.fetch) is not defined on the server, since there
+  // is no window object outside of the browser. Request acts like fetch.
+
+  // Because Tweets and Instagram posts include <script> tags for their embed
+  // scripts, which you will load separately, you need to remove them.
+  const jsonEscape = (string) => string.replace(/<script.*<\/script>/, "")
+  // Specifically, leaving the </script> tag will cause "SyntaxError:
+  // unterminated string literal" error in some browsers, including Firefox.
+
+  // If returning multiple Promises, it can be helpful to use Promise.all with
+  // an array of Promises, such as when fetching multiple social media posts.
   return Promise.all([twitterPromise, instagramPromise]).then((values) => [
-    values[0] ? values[0].html : null,
-    values[1] ? values[1].html : null,
+    values[0] ? jsonEscape(values[0].html) : null,
+    values[1] ? jsonEscape(values[1].html) : null,
   ])
+  // If a Promise doesn't exist, null is returned instead of a TypeError.
 }
